68b6d94862cb2755e753f461c13a299ed7b9d979
Illia Sernikov <illia.sernikov@gmail.com>
2017-02-12 00:20:34 +0200

    Added application repository

---
+++ backend/src/main/java/ua/sernikov/repository/ApplicationRepository.java
@@ -0,0 +1,17 @@
+package ua.sernikov.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ua.sernikov.domain.Application;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface ApplicationRepository extends CrudRepository<Application, Long> {
+
+    @Override
+    Collection<Application> findAll();
+
+    Optional<Application> findByKey(String applicationKey);
+
+    Collection<Application> findAllByOwnerKey(String ownerKey);
+}
