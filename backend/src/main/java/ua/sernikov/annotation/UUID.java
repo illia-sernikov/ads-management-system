4132a2500b5d748b0b37cc797dab41667d59f1e9
Illia Sernikov <illia.sernikov@gmail.com>
2017-02-12 16:55:01 +0200

    Deleted null constraint from UUID validator

--- backend/src/main/java/ua/sernikov/annotation/UUID.java
+++ backend/src/main/java/ua/sernikov/annotation/UUID.java
@@ -2,7 +2,6 @@ package ua.sernikov.annotation;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -12,7 +11,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 @Constraint(validatedBy = {})
-@NotNull
 @Pattern(regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
 public @interface UUID {
 
