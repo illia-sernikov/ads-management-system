082537971f3a6aaaf894a51f905c10014508b72a
Illia Sernikov <illia.sernikov@gmail.com>
2017-02-13 00:10:49 +0200

    Updated Admin page. Added form for creation of new users. Displayed all users.

--- frontend/src/app/component/admin/admin.component.ts
+++ frontend/src/app/component/admin/admin.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../../service';
+import { User } from '../../domain';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'ams-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  users: User[] = [];
+
+  private active = true;
+
+  constructor(private userService: UserService) {
+  }
 
   ngOnInit() {
+    this.userService.getAllUsers()
+        .takeWhile(() => this.active)
+        .subscribe(users => this.users = users);
+  }
+
+  ngOnDestroy(): void {
+    this.active = false;
+  }
+
+  createUser(userForm: NgForm) {
+    const newUser = userForm.value as User;
+
+    this.userService.createUser(newUser)
+        .toPromise()
+        .then(user => this.users.push(user));
+
+    userForm.reset();
   }
 
 }
